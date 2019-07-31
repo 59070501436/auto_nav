2413777d551c6ebb2fc799d2145688cc221fb4d3
ThomasDegallaix <thomas.degallaix@isen.yncrea.fr>
2019-07-31 17:58:47 +0200

    bbbb

--- scripts/lane_detection.py
+++ scripts/lane_detection.py
@@ -17,7 +17,7 @@ from os.path import expanduser
 #import matplotlib.animation as animation
 
 roi_x = 0
-roi_y = 400
+roi_y = 300
 max_value_H = 360/2
 max_value = 255
 low_H = 0
@@ -62,7 +62,7 @@ def inv_perspective_warp(img, dst_size, src, dst):
     warped = cv2.warpPerspective(img, M, dst_size)
     return warped, M
 
-def sliding_window(img, nwindows=8, margin=50, minpix = 1, draw_windows=True):
+def sliding_window(img, nwindows=15, margin=50, minpix=1, draw_windows=True):
     global left_a, left_b, left_c,right_a, right_b, right_c
     left_fit_= np.empty(3)
     right_fit_ = np.empty(3)
@@ -126,9 +126,9 @@ def sliding_window(img, nwindows=8, margin=50, minpix = 1, draw_windows=True):
       # Draw the windows on the visualization image
       if draw_windows == True:
         cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
-            (100,255,255), 3)
+            (255,0,0), 3)
         cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
-            (100,255,255), 3)
+            (255,0,0), 3)
 
     # Concatenate the arrays of indices
     left_lane_inds = np.concatenate(left_lane_inds)
@@ -165,7 +165,7 @@ def sliding_window(img, nwindows=8, margin=50, minpix = 1, draw_windows=True):
     left_fitx = left_fit_[0]*ploty**2 + left_fit_[1]*ploty + left_fit_[2]
     right_fitx = right_fit_[0]*ploty**2 + right_fit_[1]*ploty + right_fit_[2]
 
-    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 100]
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [0, 100, 255] #[255, 0, 100]
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 100, 255]
 
     return out_img, (left_fitx, right_fitx), (left_fit_, right_fit_), ploty
@@ -199,25 +199,60 @@ def vid_pipeline(cap):
     # Perspective warp
     rheight, rwidth = thresh.shape[:2]
     dst_size =(rheight,rwidth)
-    src=np.float32([(0.15,0), (0.85,0), (0.15,1), (0.85,1)])
+    src=np.float32([(0.05,0), (1,0), (0.05,1), (1,1)])
     dst=np.float32([(0,0), (1,0), (0,1), (1,1)])
     warped_img, M  = perspective_warp(thresh, dst_size, src, dst)
 
     # Sliding Window Search
     out_img, curves, lanes, ploty = sliding_window(warped_img)
 
-    # Fitted curves as points
-    warp_zero = np.zeros_like(out_img).astype(np.uint8)
-    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-    left = np.array([np.transpose(np.vstack([curves[0], ploty]))])
-    right = np.array([np.flipud(np.transpose(np.vstack([curves[1], ploty])))])
-    points = np.hstack((left, right))
+    #plt.imshow(out_img)
+    #plt.plot(curves[0], ploty, color='yellow', linewidth=1)
+    #plt.plot(curves[1], ploty, color='yellow', linewidth=1)
+    #plt.plot((curves[0]+curves[1]/2), ploty, color='red', linewidth=1)
+    #plt.show()
+
 
     # Fitted curves as points
-    #color_img = np.zeros_like(Roi)
-    #cv2.fillConvexPoly(out_img, np.int_(points), (255, 255, 0), lineType=8, shift=0)
+    leftLane = np.array([np.transpose(np.vstack([curves[0], ploty]))])
+    rightLane = np.array([np.flipud(np.transpose(np.vstack([curves[1], ploty])))])
+    points = np.hstack((leftLane, rightLane))
 
-    dst_size =(rwidth, rheight) #+roi_x +roi_y
+    #curve = leftLane
+    #curve = np.array([[0, 0], [5, 5], [50, 50], [100, 100]])
+    #print leftLane[0]
+    curve = np.array([curves[0], ploty])
+    print curve[0][0], curve[0][1]
+
+    #cv2.polylines(out_img, [curve], 1, (0,255,255))
+    #curve.convertTo(curve, CV_32S) #adapt type for polylines
+
+    midPoints = []
+
+    if len(leftLane[0]) > len(rightLane[0]):
+        longerLane = leftLane
+        shorterLane = rightLane
+    else:
+        longerLane = rightLane
+        shorterLane = leftLane
+        #adjust = -adjust
+
+    # As long as there are still points in each lane, calculate the midpoint of lane by averaging corresponding
+    # points
+    for i in xrange(0, len(shorterLane[0])):
+            sumPoint = np.add(longerLane[0][i], shorterLane[0][i])
+            avgPoint = np.divide(sumPoint, 2)
+            midPoints.append(avgPoint)
+            cv2.circle(out_img, (int(avgPoint[0]), int(avgPoint[1])), 8, (0, 255, 255), -1)
+    # Once you run out of corresponding points, simply horizontally offset the remaining points
+    #for i in xrange(len(shorterLane[0]), len(longerLane[0])):
+        #midPoints.append(np.add(longerLane[0][i], [0, adjust]))
+
+    #midPoints = [list(imap(int, midPoint)) for midPoint in midPoints]
+    #m_x, m_y = midPoints[0]
+
+
+    dst_size =(rwidth, rheight)
     invwarp, Minv = inv_perspective_warp(out_img, dst_size, dst, src)
 
     # Combine the result with the original image
@@ -230,7 +265,7 @@ def vid_pipeline(cap):
 def lane_detector():
   rospy.init_node('lane_detector', anonymous=True)
 
-  home = expanduser("~/ICRA_2020/wheel_tracks.avi")
+  home = expanduser("~/ICRA_2020/wheel_tracks_ct.avi")
   cap = cv2.VideoCapture(home)
 
   # Check if camera opened successfully
@@ -247,16 +282,15 @@ def lane_detector():
       cv2.startWindowThread()
       cv2.namedWindow('preview', cv2.WINDOW_NORMAL)
       cv2.resizeWindow('preview', 800,800)
-      #cv2.imshow('Frame', output)
+      cv2.imshow('preview', warp_img)
 
       #fheight, fwidth = output.shape[:2]
       #print warp_img.shape, output.shape
       #warp_img = cv2.resize(warp_img,(int(fwidth),int(fheight)))
       #numpy_horizontal = np.hstack((warp_img, output))
-      cv2.imshow('preview', warp_img)
-      cv2.namedWindow('preview1', cv2.WINDOW_NORMAL)
-      cv2.resizeWindow('preview1', 800,800)
-      cv2.imshow('preview1', output)
+      #cv2.namedWindow('preview1', cv2.WINDOW_NORMAL)
+      #cv2.resizeWindow('preview1', 800,800)
+      #cv2.imshow('preview1', output)
 
       # Press Q on keyboard to  exit
       if cv2.waitKey(25) & 0xFF == ord('q'):
