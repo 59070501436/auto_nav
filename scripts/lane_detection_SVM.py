ae1d220869aa54a98f48d587dc6b989f87a16164
Vignesh <vignesh.raja199@gmail.com>
2019-09-03 23:21:23 +0200

    aa

--- scripts/lane_detection_SVM.py
+++ scripts/lane_detection_SVM.py
@@ -142,8 +142,37 @@ if __name__ == '__main__':
                             hog_channel=hog_channel, hist_feat=hist_feat,
                             spatial_feat=spatial_feat, hog_feat=hog_feat)
 
-   while not rospy.is_shutdown():
-     lf = lane_finder_SVM(rgb_img)
+   X = np.vstack((car_features, notcar_features)).astype(np.float64)
+   # Fit a per-column scaler
+   X_scaler = StandardScaler().fit(X)
+   # Apply the scaler to X
+   scaled_X = X_scaler.transform(X)
+
+   # Define the labels vector
+   y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+   # Split up data into randomized training and test sets
+   rand_state = np.random.randint(0, 100)
+   X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
+
+   print('Using:',orient,'orientations',pix_per_cell,
+        'pixels per cell and', cell_per_block,'cells per block')
+   print('Feature vector length:', len(X_train[0]))
+   # Use a linear SVC
+   svc = LinearSVC()
+   # Check the training time for the SVC
+   t=time.time()
+   svc.fit(X_train, y_train)
+   t2 = time.time()
+   print(round(t2-t, 2), 'Seconds to train SVC...')
+   # Check the score of the SVC
+   print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+   # Check the prediction time for a single sample
+   t=time.time()
+
+   # while not rospy.is_shutdown():
+   #   lf = lane_finder_SVM(rgb_img)
 
      # Closes all the frames
      #cv2.waitKey(0)
