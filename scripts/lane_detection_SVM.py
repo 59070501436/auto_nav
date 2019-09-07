49c6fe913f26069b6eefb301a4a336a5c0b9b4ac
Vignesh <vignesh.raja199@gmail.com>
2019-09-07 23:14:18 +0200

    WIP: texture gabor filter

--- scripts/lane_detection_SVM.py
+++ scripts/lane_detection_SVM.py
@@ -19,20 +19,33 @@ class lane_finder_SVM():
         self.image = image
 
         # Gabor Filter Parameters
-        self.ksize = (24, 24)
-        self.sigma = 5
+        # self.ksize = (24, 24)
+        # self.sigma = 5
         self.theta = np.pi/4 #np.pi/4
-        self.lambda1 = 4
-        self.gamma = 0.25
-        self.psi = 0
+        # self.lambda1 = 4
+        self.gamma = 0.5
+        #self.psi = 0
         self.ktype = cv2.CV_32F
-        self.g_kernel = cv2.getGaborKernel(self.ksize, self.sigma, self.theta, self.lambda1, self.gamma, self.psi, self.ktype)
+
+    def nothing_1(self, val):
+        pass
+    def nothing_2(self, val):
+        pass
+    def nothing_3(self, val):
+        pass
+    def nothing_4(self, val):
+        pass
+    def nothing_5(self, val):
+        pass
+    def nothing_6(self, val):
+        pass
 
     # Our main driver function to return the segmentation of the input image.
-    def runGabor(self):
-        filtered_img = cv2.filter2D(self.image, cv2.CV_8UC3, self.g_kernel)
-        Filelocation = expanduser("~/filteredImages1.png")
-        cv2.imwrite(Filelocation, filtered_img)
+    def runGabor(self, ksize, sigma, theta, lambda1, gamma, psi):
+
+        g_kernel = cv2.getGaborKernel((ksize,ksize), sigma, self.theta, lambda1, self.gamma, psi, self.ktype)
+        filtered_img = cv2.filter2D(self.image, cv2.CV_8UC3, g_kernel)
+        return filtered_img
 
 if __name__ == '__main__':
 
@@ -40,16 +53,42 @@ if __name__ == '__main__':
    rospy.init_node('horizon_detection', anonymous=True)
 
    # Load an color image in grayscale
-   home = expanduser("~/Third_Paper/Dataset_skp/overhead_img.png") #SKP_post_harvest_dataset/Photos/SKP_6/left0000.jpg")
+   #home = expanduser("~/Third_Paper/SKP/overhead_img.png") #SKP_post_harvest_dataset/Photos/SKP_6/left0000.jpg")
+   home = expanduser("~/Third_Paper/Datasets/Frogn/frogn_001.jpg") #SKP_post_harvest_dataset/Photos/SKP_6/left0000.jpg")
    rgb_img = cv2.imread(home)
 
+   lf = lane_finder_SVM(rgb_img)
+   cv2.namedWindow('preview', cv2.WINDOW_NORMAL)
+   cv2.createTrackbar('ksize', 'preview' , 0, 50, lf.nothing_1)
+   cv2.createTrackbar('sigma', 'preview' , 0, 20, lf.nothing_2)
+   cv2.createTrackbar('theta', 'preview' , 0, 3, lf.nothing_3)
+   cv2.createTrackbar('lambda1', 'preview', 0, 20, lf.nothing_4)
+   cv2.createTrackbar('gamma', 'preview', 0, 1, lf.nothing_5)
+   cv2.createTrackbar('psi', 'preview', 0, 20, lf.nothing_6)
 
    while not rospy.is_shutdown():
-      lf = lane_finder_SVM(rgb_img)
-      lf.runGabor()
-     # Closes all the frames
-     #cv2.waitKey(0)
-     #cv2.destroyAllWindows()
+
+      # get current positions of four trackbars
+      ksize = cv2.getTrackbarPos('ksize','preview')
+      sigma = cv2.getTrackbarPos('sigma','preview')
+      theta = cv2.getTrackbarPos('theta','preview')
+      lambda1 = cv2.getTrackbarPos('lambda1','preview')
+      gamma = cv2.getTrackbarPos('gamma','preview')
+      psi = cv2.getTrackbarPos('psi','preview')
+      print ksize, sigma, lambda1, psi
+
+      img_texture = lf.runGabor(ksize,sigma,theta,lambda1,gamma,psi) # Function for texture filter
+
+      # Closes all the frames
+      # cv2.resizeWindow('preview', 800,800)
+      cv2.imshow('preview', img_texture)
+      k = cv2.waitKey(1) & 0xFF
+      if k == 27:
+        break
+
+   cv2.destroyAllWindows()
+      #Filelocation = expanduser("~/filteredImages.png")
+      #cv2.imwrite(Filelocation, filtered_img)
 
   except rospy.ROSInterruptException:
    pass
